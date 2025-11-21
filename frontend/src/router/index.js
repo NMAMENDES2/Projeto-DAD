5bdbe2156452b917c69ff3fce072869336aeeaef
Gabriel Santos <gabriel.sfarto@gmail.com>
2025-11-21 20:09:38 +0000

    esquecime da rota

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import Home from '../pages/Home.vue'
 import Register from '@/components/auth/Register.vue'
 import Login from '@/components/auth/Login.vue'
 import Game from '@/components/game/Game.vue'
+import Transactions from '@/pages/Transactions.vue'
+
 
 const router = createRouter({
   history: createWebHistory(), 
@@ -22,6 +24,12 @@ const router = createRouter({
       name: 'login',
       component: Login,
     },
+    {
+    path: '/transactions',
+    name: 'Transactions',
+    component: Transactions,
+    meta: { requiresAuth: true }
+  }
   ],
 })
 
